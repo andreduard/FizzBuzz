9218b6c536b24e5f96ff7b1133f28d0e47147905
andreduard <andreguti333@gmail.com>
2016-06-08 10:33:32 -0600

    Comentarios Model.java
    Se comento el código

--- src/fizzbuzz/Model.java
+++ src/fizzbuzz/Model.java
@@ -16,23 +16,23 @@ public void DoFizzBuzz(){
     for(int i=1 ; i <= 100; i++) {
         
                       
-        boolean is3 = i % 3 == 0;   
-        boolean is5 = i % 5 == 0;  
+        boolean is3 = i % 3 == 0;		//GIVE IS3 THE VALUE OF TRUE IF IS MOD 3 
+        boolean is5 = i % 5 == 0;		//GIVE IS5 THE VALUE OF TRUE IF IS MOD 5  
         
         
-        if (is3 && is5)       
+        if (is3 && is5)       	//IF BOTH ARE TRUE PRINT FIZZBUZZ
         {
             System.out.println("FizzBuzz");
         }       
-        else if (is3)         
+        else if (is3)         	//IF IS TRUE PRINT FIZZ
         {
             System.out.println("Fizz");
         }
-        else if (is5)      
+        else if (is5)      		//IF IS TRUE PRINT BUZZ
         {
             System.out.println("Buzz");
         }
-        else                    
+        else                    //IF NOT TRUE PRINT THE NUMBER
         {
             System.out.println(i);
         }
